d56bcdfb4de06e654eb6e5e8c5c835238b02e766
j <137029997+Jagacool@users.noreply.github.com>
2023-08-19 23:17:04 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from '../components/Card'
+import Header from '../components/Header'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='banner'>
+            <h1>Template Web </h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu luctus ipsum, rhoncus semper magna. Nulla nec magna sit amet sem interdum condimentum.</p>
+        </div>
+<Header/>
+<Card/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,16 @@
+
+import { useNavigate } from "react-router-dom";
+import error from "../assets/404.png";
+
+const NotFound = () => {
+  const navigate = useNavigate()
+  return (
+    <div className="error">
+      <h1>PLEASE ENTER VALID PAGE URL!!!</h1>
+      <img src={error} alt="" />
+      <button className="buton" onClick={() => navigate("/")} >Home</button>
+    </div>
+  );
+};
+
+export default NotFound;
