4c7245c47cbd44c423ee17979c2ff609dbdd2dab
j <137029997+Jagacool@users.noreply.github.com>
2023-08-21 13:15:51 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import React from 'react';
 import img1 from "../assets/logo_brush.png";
 import img2 from "../assets/logo_css.png";
 import img3 from "../assets/logo_html.png";
@@ -7,42 +7,25 @@ const Card = () => {
   return (
     <div className="cont">
       <div className="cart">
-        <Link to="/html">
-          <img  className="img" src={img3} alt="html" />
-          
-        </Link>
-        <h5>HTML5 Markup</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies</p>
-        
-        <Link to="/html">
-        <button className="buton">Click here</button>
-          
-        </Link>
+        <img className="img" src={img1} alt="" />
+        <h3>Graphic Design</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptates...</p>
+        <a href="/logo" className="buton">Learn More</a>
       </div>
-      <div  className="cart">
-        <Link to="/css">
-          <img className="img" src={img2} alt="css" />
-        </Link>
-        <h5>CSS3 Styling</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies</p>
-        <Link to="/html">
-        <button className="buton">Click here</button>
-          
-        </Link>
+      <div className="cart">
+        <img className="img" src={img2} alt="" />
+        <h3>CSS3 Styling</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptates...</p>
+        <a href="/css" className="buton">Learn More</a>
       </div>
-      <div  className="cart">
-        <Link to="/logo">
-          <img className="img" src={img1} alt="design" />
-        </Link>
-        <h5>Graphic Design</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies</p>
-        <Link to="/html">
-        <button className="buton">Click here</button>
-          
-        </Link>
+      <div className="cart">
+        <img className="img" src={img3} alt="" />
+        <h3>HTML5 Markup</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptates...</p>
+        <a href="/html" className="buton">Learn More</a>
       </div>
     </div>
   );
-};
+}
 
 export default Card;
